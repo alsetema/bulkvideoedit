53f16800afeaeab7b2e840506da726e76cdddd06
A. Tellez <alsetema@gmail.com>
2020-10-07 22:52:51 +0200

    changed library to another one, need to figure out why the URI doesnt work for the file that is picked

--- app/src/main/java/com/example/bulkvideoeditor/MainActivity.java
+++ app/src/main/java/com/example/bulkvideoeditor/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 
+import android.net.Uri;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,20 +15,22 @@ import android.widget.VideoView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import nl.bravobit.ffmpeg.FFmpeg;
-import nl.bravobit.ffmpeg.FFprobe;
+import com.arthenica.mobileffmpeg.FFmpeg;
+import com.arthenica.mobileffmpeg.FFprobe;
+import com.arthenica.mobileffmpeg.MediaInformation;
 
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private FFmpeg ffmpeg = null;
+
+
     private VideoView vw1;
     private TextView clipInfoView;
     private Button filePickButton;
     final int CODE_MULTIPLE_VIDEO = 1;
-    private FFprobe ffprobe = null;
-    final Toast errorToast = Toast.makeText(this, "", Toast.LENGTH_LONG);
+
+    private Toast errorToast;
     String[] texts = {"super cool", "amazing", "seems to be working!", "nice", "69", "420", "poop", "this is supposed to be a really long text to see how it would wrap arround the screen or what happens"};
     Random rand = new Random();
 
@@ -35,12 +38,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        if(errorToast == null) {
+            errorToast = Toast.makeText(MainActivity.this, "", Toast.LENGTH_LONG);
+        }
         filePickButton = findViewById(R.id.pickfile);
         vw1 = findViewById(R.id.vw1);
         clipInfoView = findViewById(R.id.clipinfo);
 
-        loadLibs();
+
 
         filePickButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    //I changed the library thing to a new one so I need to re-write everythign the other library did
 
     public void choseFile() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -67,49 +74,27 @@ public class MainActivity extends AppCompatActivity {
             ClipData cd = data.getClipData();
 
             if (cd != null) {
-                vw1.setVideoURI(cd.getItemAt(rand.nextInt(cd.getItemCount())).getUri());
+                Uri testUri = cd.getItemAt(rand.nextInt(cd.getItemCount())).getUri();
+                probeVid(testUri);
+                vw1.setVideoURI(testUri);
                 vw1.start();
             }
         }
 
     }
 
-    private void probeVid
+    private void probeVid(Uri videoURI) {
+        // I think this was supposed to get the information on the video to check the resolution first, and then decide how to square it.
+        //need to figure out how the fucking ffmeg library works
 
-    public void ffprobeLoad() {
-        Context appContext = getApplicationContext();
-        //check for ffprobe
-        if(FFprobe.getInstance(this).isSupported()) {
-            ffprobe = ffprobe.getInstance(this);
-        } else {
-            String text = "Error loading FFprobe library";
-            errorToast.setText(text);
-            errorToast.show();
-            finish();
-        }
+        //checking what the uri is a string
+        System.out.println("the URI is: " + videoURI.toString());
+        MediaInformation info = FFprobe.getMediaInformation(videoURI.toString());
     }
 
-    public void ffmpegLoad() {
-        Context appContext = getApplicationContext();
-        // first check for ffmpeg availability
-        if (FFmpeg.getInstance(this).isSupported()) {
-            ffmpeg = FFmpeg.getInstance(this);
 
-            //String text = "Success loading the FFmpeg Library!";
-           // final Toast loadSuccess = Toast.makeText(appContext,text,Toast.LENGTH_LONG);
-            //loadSuccess.show();
-            //testing the exiting of the app anyway
-            //sleep(1000);
-            //finish();
 
-        } else {
-            String text = "Error Loading the FFmpeg Library!";
-            errorToast.setText(text);
-            errorToast.show();
-            finish();
-        }
 
-    }
 
     private void sleep(long ms) {
         try {
@@ -119,9 +104,5 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void loadLibs() {
-        ffmpegLoad();
-        ffprobeLoad();
-    }
 
 }
