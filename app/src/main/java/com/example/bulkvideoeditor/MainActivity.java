04245a7fc41dec3a42ba1e808ac461a09868edf6
A. Tellez <alsetema@gmail.com>
2020-10-27 22:15:30 +0100

    fix issue with the files. making own copy of files in cache directory. can Use ffmpeg with it

--- app/src/main/java/com/example/bulkvideoeditor/MainActivity.java
+++ app/src/main/java/com/example/bulkvideoeditor/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.bulkvideoeditor;
 
 
 import android.content.ClipData;
-import android.content.Context;
 import android.content.Intent;
 
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,6 +20,7 @@ import com.arthenica.mobileffmpeg.FFprobe;
 import com.arthenica.mobileffmpeg.MediaInformation;
 
 
+import java.io.*;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
     private VideoView vw1;
     private TextView clipInfoView;
     private Button filePickButton;
-    final int CODE_MULTIPLE_VIDEO = 1;
-
+    final int CODE_MULTIPLE_VIDEO = 2;
+    // FOR NOW ONLY MULTIPLE FILE, final int CODE_SINGLE_VIDEO = 1;
     private Toast errorToast;
     String[] texts = {"super cool", "amazing", "seems to be working!", "nice", "69", "420", "poop", "this is supposed to be a really long text to see how it would wrap arround the screen or what happens"};
     Random rand = new Random();
@@ -75,9 +76,10 @@ public class MainActivity extends AppCompatActivity {
 
             if (cd != null) {
                 Uri testUri = cd.getItemAt(rand.nextInt(cd.getItemCount())).getUri();
+                //this URI provided by the file picker doesnt seem to be too good for the library?
                 probeVid(testUri);
-                vw1.setVideoURI(testUri);
-                vw1.start();
+                //vw1.setVideoURI(testUri);
+                //vw1.start();
             }
         }
 
@@ -88,10 +90,44 @@ public class MainActivity extends AppCompatActivity {
         //need to figure out how the fucking ffmeg library works
 
         //checking what the uri is a string
-        System.out.println("the URI is: " + videoURI.toString());
-        MediaInformation info = FFprobe.getMediaInformation(videoURI.toString());
-    }
+        System.out.println("the URI path is: " + videoURI.getPath());
+        System.out.println("the URI string is: " + videoURI.toString());
+        // so the URI doesnt work becuase of security. need to use content resolver
+        //to make my own copy of the file thta I control, and put that one into ffmpeg (i know
+        //the location of that one)
+        //hardcoded thing shit uri value for test
+        try {
+            InputStream fileInputFromURI = getContentResolver().openInputStream(videoURI);
+            System.out.println(getCacheDir().getAbsolutePath());
+            File testFile = new File(getCacheDir(), "testOutputVideo.mp4");
+            OutputStream fileOutput = new FileOutputStream(testFile);
+            byte[] buffer = new byte[10000*1024];
+            int read = 0;
+            while (read != -1) {
+                //read into the buffer, and put number of byter read into "read"
+                read = fileInputFromURI.read(buffer);
+                //write into the file out, from the buffer, the number of bytes previously read, no offset
+                if(read != -1) {
+                    fileOutput.write(buffer, 0, read);
+                }
 
+            }
+            fileOutput.flush();
+            fileOutput.close();
+            System.out.println("Test file path thing " + testFile.getPath());
+            MediaInformation info = FFprobe.getMediaInformation(testFile.getPath());
+            System.out.println(info.getFormat());
+            vw1.setVideoURI(Uri.fromFile(testFile));
+            vw1.start();
+        }catch (FileNotFoundException e) {
+            System.out.println("what the fuck this failed :))))");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
 
 
