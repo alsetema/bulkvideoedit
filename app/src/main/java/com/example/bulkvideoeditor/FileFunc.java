5e44f2e62de5cd16d52c34b2506dec9d836a4b62
alsetema <alsetema@gmail.com>
2021-04-24 00:10:23 +0200

    modify function to pick on eor more files at once, will make switch to new library that is wrapper

---
+++ app/src/main/java/com/example/bulkvideoeditor/FileFunc.java
@@ -0,0 +1,50 @@
+package com.example.bulkvideoeditor;
+
+import android.content.Context;
+import android.net.Uri;
+import java.math.*;
+import java.io.*;
+import java.util.Random;
+
+
+public class FileFunc {
+
+    //error handling will be done in exceptions no int shit
+    protected static String uriToFile(Uri inputURI, String path, Context ct) throws IOException, FileNotFoundException {
+
+        InputStream fileIS = ct.getContentResolver().openInputStream(inputURI);
+
+        File outFile = genTempFile(path);
+        OutputStream fileOutput = new FileOutputStream(outFile);
+        byte[] buffer = new byte[10000*1024];
+        int read;
+        do {
+            //read into the buffer, and put number of byter read into "read"
+            read = fileIS.read(buffer);
+            if (read != -1) {
+                //write into the file out, from the buffer, the number of bytes previously read, no offset
+                fileOutput.write(buffer,0,read);
+            }
+        } while (read != -1);
+        fileOutput.flush();
+        fileOutput.close();
+        //returns in a string the name of the temp file if you need to do anything in it
+        return outFile.getName();
+
+    }
+
+    private static File genTempFile(String path) {
+        Random rd = new Random();
+        int bound = 100000;
+        String fileName = rd.nextInt(bound) + ".tmp";
+        File tempFile = new File(path,fileName);
+        while ((tempFile.exists() && tempFile.isFile()) || tempFile.isDirectory()) {
+            fileName = rd.nextInt(bound) + ".tmp";
+            tempFile = new File(path,fileName);
+        }
+        return tempFile;
+    }
+
+
+
+}
--- app/src/main/java/com/example/bulkvideoeditor/MainActivity.java
+++ app/src/main/java/com/example/bulkvideoeditor/MainActivity.java
@@ -5,17 +5,15 @@ import android.content.ClipData;
 import android.content.Intent;
 
 import android.net.Uri;
-import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.arthenica.mobileffmpeg.FFmpeg;
 import com.arthenica.mobileffmpeg.FFprobe;
 import com.arthenica.mobileffmpeg.MediaInformation;
 
@@ -28,8 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
     private VideoView vw1;
     private TextView clipInfoView;
-    private Button filePickButton;
-    final int CODE_MULTIPLE_VIDEO = 2;
+    private Button filePickMultiButton;
+    final int CODE_PICKED_VIDEO = 2;
+    final int CODE_SINGLE_VIDEO = 1;
     // FOR NOW ONLY MULTIPLE FILE, final int CODE_SINGLE_VIDEO = 1;
     private Toast errorToast;
     String[] texts = {"super cool", "amazing", "seems to be working!", "nice", "69", "420", "poop", "this is supposed to be a really long text to see how it would wrap arround the screen or what happens"};
@@ -39,19 +38,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(errorToast == null) {
+        if (errorToast == null) {
             errorToast = Toast.makeText(MainActivity.this, "", Toast.LENGTH_LONG);
         }
-        filePickButton = findViewById(R.id.pickfile);
+        filePickMultiButton = findViewById(R.id.pickfile);
         vw1 = findViewById(R.id.vw1);
         clipInfoView = findViewById(R.id.clipinfo);
 
 
-
-        filePickButton.setOnClickListener(new View.OnClickListener() {
+        filePickMultiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               choseFile();
+                choseFile();
             }
         });
 
@@ -60,30 +58,38 @@ public class MainActivity extends AppCompatActivity {
     //I changed the library thing to a new one so I need to re-write everythign the other library did
 
     public void choseFile() {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        intent.setType("video/mp4");
-        startActivityForResult(intent, CODE_MULTIPLE_VIDEO);
+        Intent filePicker = new Intent(Intent.ACTION_GET_CONTENT);
+        filePicker.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        filePicker.setType("video/mp4");
+        startActivityForResult(filePicker, CODE_PICKED_VIDEO);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-
-        if(requestCode == CODE_MULTIPLE_VIDEO && resultCode == RESULT_OK) {
+        //this can actually make me pick only one so I am happy
+        if (requestCode == CODE_PICKED_VIDEO && resultCode == RESULT_OK && data != null) {
+            Uri testUri = null;
             ClipData cd = data.getClipData();
 
+
             if (cd != null) {
-                Uri testUri = cd.getItemAt(rand.nextInt(cd.getItemCount())).getUri();
+                //checking if more than one file was picked and then chosing one randomly
+                testUri = cd.getItemAt(rand.nextInt(cd.getItemCount())).getUri();
                 //this URI provided by the file picker doesnt seem to be too good for the library?
+
+            } else if (cd == null) {
+                //if the content data is null it means that the thing is PROBABLY only one video rather than an array of videos, could have
+                //an array to hold all of these
+                testUri = data.getData();
+            }
+            if(testUri != null) {
                 probeVid(testUri);
-                //vw1.setVideoURI(testUri);
-                //vw1.start();
             }
         }
-
     }
+
 
     private void probeVid(Uri videoURI) {
         // I think this was supposed to get the information on the video to check the resolution first, and then decide how to square it.
@@ -97,39 +103,28 @@ public class MainActivity extends AppCompatActivity {
         //the location of that one)
         //hardcoded thing shit uri value for test
         try {
-            InputStream fileInputFromURI = getContentResolver().openInputStream(videoURI);
-            System.out.println(getCacheDir().getAbsolutePath());
-            File testFile = new File(getCacheDir(), "testOutputVideo.mp4");
-            OutputStream fileOutput = new FileOutputStream(testFile);
-            byte[] buffer = new byte[10000*1024];
-            int read = 0;
-            while (read != -1) {
-                //read into the buffer, and put number of byter read into "read"
-                read = fileInputFromURI.read(buffer);
-                //write into the file out, from the buffer, the number of bytes previously read, no offset
-                if(read != -1) {
-                    fileOutput.write(buffer, 0, read);
-                }
-
-            }
-            fileOutput.flush();
-            fileOutput.close();
-            System.out.println("Test file path thing " + testFile.getPath());
-            MediaInformation info = FFprobe.getMediaInformation(testFile.getPath());
+            String filename = FileFunc.uriToFile(videoURI, getCacheDir().toString(), this);
+            String filepath = getCacheDir() + "/" + filename;
+            Log.i("file path thing", filepath);
+            //System.out.println("file path thing "+ filepath);
+            MediaInformation info = FFprobe.getMediaInformation(filepath);
             System.out.println(info.getFormat());
-            vw1.setVideoURI(Uri.fromFile(testFile));
+
+            vw1.setVideoURI(Uri.fromFile(new File(filepath)));
             vw1.start();
-        }catch (FileNotFoundException e) {
-            System.out.println("what the fuck this failed :))))");
+            //dont print stuff, use logcat
+        } catch (FileNotFoundException e) {
+            Log.e("EXCEPTION", "what the fuck this failed :)))) (file not found)");
             e.printStackTrace();
         } catch (IOException e) {
+            Log.e("EXCEPTION", "this is a IO exception thrown at trying to uritofile");
             e.printStackTrace();
+        } catch (Exception e) {
+            Log.e("EXCEPTION,RANDOM", "you're fucked, random exception");
         }
 
 
     }
-
-
 
 
     private void sleep(long ms) {
